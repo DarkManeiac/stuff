660c9a994bb74512688f9e64d38c88903bebf8ef
quinlanmorake <qmorake@yandex.ru>
2017-01-25 18:58:09 +0000

    Inital adding pointers in

--- Functions/Functions/Functions.cpp
+++ Functions/Functions/Functions.cpp
@@ -9,9 +9,18 @@ int multiplication(int a, int b) {
 	return r;
 }
 
+int* multiplication_with_pointers(int a, int b) {
+  int* r;
+  &r = a * b;
+  return r;
+}
+
 int main() {
 	int z;
 	z = multiplication(4, 3);
+
+  int zPointer = multiplication_with_pointers(4, 3);
+  
 	cout << "Result is: " << z << endl;
 	system("pause");
   return 0;
