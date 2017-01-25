a6e53e974ea667532cbd0fa7156d04d253e67ac9
quinlanmorake <qmorake@yandex.ru>
2017-01-25 18:52:12 +0000

    Initial push

--- Functions/Functions/Functions.cpp
+++ Functions/Functions/Functions.cpp
@@ -1,24 +1,19 @@
-// Functions.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
 
 using namespace std;
 
-int multiplication(int a, int b)
-{
+int multiplication(int a, int b) {
 	int r;
 	r = a * b;
 	return r;
 }
 
-int main()
-{
+int main() {
 	int z;
 	z = multiplication(4, 3);
-	cout << "Rezult is: " << z << endl;
+	cout << "Result is: " << z << endl;
 	system("pause");
-    return 0;
+  return 0;
 }
 
